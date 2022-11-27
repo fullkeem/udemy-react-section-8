b1e58229a07d8b43eea928adb7a8b1515f4c13cc
choongmankim <cm730712@gmail.com>
2022-11-27 15:23:11 +0900

    Add: user 정보를 입력 후 state 저장 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/users/AddUser";
+import UserList from "./components/users/UserList";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsers((prevUsers) => {
+      return [
+        ...prevUsers,
+        { id: Math.random().toString(), name: uName, age: uAge },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={users} />
     </div>
   );
 }
--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
 
-const AddUser = () => {
+const AddUser = ({ onAddUser }) => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+
+    if (name.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+
+    if (+age < 1) {
+      return;
+    }
+
+    onAddUser(name, age);
+    setName("");
+    setAge("");
   };
+
+  const idHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const ageHandler = (e) => {
+    setAge(e.target.value);
+  };
+
   return (
     <Card>
       <form className={styles.input} onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input id='username' type='text' value={name} onChange={idHandler} />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' />
+        <input id='age' type='number' value={age} onChange={ageHandler} />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
